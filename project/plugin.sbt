fa89769e61a6cb47a518a57cd988559b5316b831
prantik <prantik@klout.com>
2014-03-03 16:37:42 -0800

    add sbt assembly to project

--- project/plugin.sbt
+++ project/plugin.sbt
@@ -1,6 +1,8 @@
 // Scalariform to autoformat code
 addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.2.1")
 
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.10.2")
+
 
 resolvers += "Akka Repository" at "http://repo.akka.io/releases/"
 
--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -20,9 +20,9 @@ object SimpleApp {
   def main(args: Array[String]) {
 
     configureTwitterCredentials
-    
+
     val serverLocation = if (args.length > 0) args(0)
-      else "local[4]"
+    else "local[4]"
 
     val ssc = new StreamingContext(
       serverLocation,
